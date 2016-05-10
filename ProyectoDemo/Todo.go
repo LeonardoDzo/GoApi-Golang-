2868fa02e62ee6cdbdb4c8ceeb6f77b12ca4bbcb
Leonardo Durazo <leo.durazod@gmail.com>
2016-05-09 22:16:33 -0700

    added project

---
+++ ProyectoDemo/Todo.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"time"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Todo struct {
+	Id        bson.ObjectId  `json:"_id" bson:"_id"`
+	Name      string    `json:"name" bson:"name"`
+	Completed bool      `json:"completed" bson:"completed"`
+	Due       time.Time `json:"due" bson:"due"`
+}
+
+type Todos []Todo
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GoApi-Golang-
+
+This is a project in the language  GOLANG 
+which was developed to learn some Go 
+and learn the operation of routing 
+with the Gorilla web tool
